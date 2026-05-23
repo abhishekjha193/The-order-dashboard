1cc1dcef38894f46dd41e382304d0cfba64cb438
Abhishek Jha <abhibj2003@gmail.com>
2026-05-24 00:08:15 +0530

    update project

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,67 +3,92 @@ import OrderTabs from "../components/order/OrderTabs";
 import OrderDetails from "../components/order/OrderDetails";
 import OrderBreakdown from "../components/order/OrderBreakdown";
 import CustomerDetails from "../components/order/CustomerDetails";
-import { leftPanelData, activityTimelineData } from "../data/mockData";
+
+import {
+  leftPanelData,
+  activityTimelineData,
+} from "../data/mockData";
+
 import StatusBadge from "../components/common/StatusBadge";
 
 function Dashboard() {
   return (
-    <div className="dashboard-wrapper">
-      <div className="left-panel">
-        <div className="left-top">
-          {leftPanelData.map((item, index) => (
-            <div className="order-box" key={index}>
-              <p>{item.title}</p>
+    <div className="dashboard-page">
 
-              <h4>{item.value}</h4>
-            </div>
-          ))}
+      <OrderSummary />
 
-          <div className="order-box">
-            <p>Status</p>
+      <div className="dashboard-content-area">
 
-            <StatusBadge status="Process" />
-          </div>
-        </div>
-        <div className="left-timeline">
-          {activityTimelineData.map((item) => (
-            <div className="left-timeline-item" key={item.id}>
-              <div className="timeline-icon">
-                <i className="ri-file-list-3-line"></i>
-              </div>
+        <div className="left-panel">
 
-              <div className="timeline-info">
-                <h5>
-                  {item.status} <span>|{item.code}</span>
-                </h5>
+          <div className="left-top">
 
+            {leftPanelData.map((item, index) => (
+              <div className="order-box" key={index}>
                 <p>{item.title}</p>
-
-                <small>
-                  {item.date} | {item.time}
-                </small>
+                <h4>{item.value}</h4>
               </div>
+            ))}
+
+            <div className="order-box">
+              <p>Status</p>
+              <StatusBadge status="Process" />
             </div>
-          ))}
+
+          </div>
+
+          <div className="left-timeline">
+
+            {activityTimelineData.map((item) => (
+              <div className="left-timeline-item" key={item.id}>
+
+                <div className="timeline-icon">
+                  <i className="ri-file-list-3-line"></i>
+                </div>
+
+                <div className="timeline-info">
+
+                  <h5>
+                    {item.status} <span>|{item.code}</span>
+                  </h5>
+
+                  <p>{item.title}</p>
+
+                  <small>
+                    {item.date} | {item.time}
+                  </small>
+
+                </div>
+
+              </div>
+            ))}
+
+          </div>
+
         </div>
+
+        <div className="right-dashboard">
+
+          <OrderTabs />
+
+          <div className="main-dashboard">
+
+            <OrderDetails />
+
+            <OrderBreakdown />
+
+            <CustomerDetails />
+            
+            <br /><br /><br /><br />
+
+          </div>
+
+        </div>
+
       </div>
 
-      <div className="main-dashboard">
-        <OrderSummary />
-
-        <OrderTabs />
-
-        <OrderDetails />
-
-        <OrderBreakdown />
-
-        <CustomerDetails />
-
-      </div>
     </div>
   );
 }
 
 export default Dashboard;
-
-
